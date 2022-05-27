79a66d2a2c4af981e9002e302ac195b43b3653fc
MarcioRMed <contato.microinfo@gmail.com>
2022-05-27 18:18:16 -0300

    ajustes

--- main.js
+++ main.js
@@ -6,7 +6,71 @@ onScroll()
 function onScroll(){
     showNavOnScroll()
     showBackToTopButtonOnScroll()
+
+    activateMenuAtCurrentSection()
+
 }
+
+
+
+
+function activateMenuAtCurrentSection(){
+    
+    // descobre alinha central da tela
+    const targetLine = scrollY + innerHeight / 2
+
+    console.log(targetLine)
+
+    // verifica se a seção passsou da linha
+    // quais dados vou precisar?
+    const sectionTop = home.offsetTop
+    
+    const sectionHeight = home.offsetHeight
+    
+    console.log(' altura total section = final do home ' + sectionHeight)
+    
+    
+    
+    
+    console.log('inicio do top ' + sectionTop) //
+
+
+}
+
+
+/**
+ *  .innerHeight - altura visivel do navegador  
+ *  .offsetTop - mostra a posicao do inicio da secção
+ * 
+
+
+
+
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function showNavOnScroll(){
     if (scrollY > 0){
